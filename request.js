f8e78246b1e661c30fe565cae2da8c5fc80d0214
drichard1989 <drichard1989@icloud.com>
2017-01-16 14:00:18 -0600

    I have created the files, installed the packages for request and twitter. So far, I have made a succesful request to OMDB with it being in the global scope, and not having switch cases implemented. I added a switch case, and tried to integrate the code into a function to call that function when the case is used, but the data will not successfully return with an OMDB call.

---
+++ request.js
@@ -0,0 +1,56 @@
+// We include the request packages for OMDB and Twitter.
+
+var request = require("request");
+var Twitter = require("twitter");
+
+
+var action = process.argv[2];
+var input = process.argv[3];
+
+// Lets create a switch case for multiple requests from the same document.
+
+switch (input){
+	case "twitter":
+		tweetReturns();
+		break;
+
+	case "OMDB":
+		OMDBRequest();
+		break;
+};
+
+
+// This is the function that requests the information from OMDB
+
+function OMDBRequest(){
+	// Setting the variable for URL integration
+	var movieTitle = "";
+
+	// Creating a for loop so I can input multiple word movie titles...
+	for (i=3; i<process.argv.length; i++){
+	movieTitle = movieTitle + process.argv[i] + " ";
+	};	
+
+	// Setting the variable for the API call 
+	var omdbAPIURL = "http://omdbapi.com?t=" + movieTitle + "&r=json";
+
+	// Requesting the information from OMDB
+	request(omdbAPIURL, function (error, response, body) {
+		// If no error, and response code is 200, which means works fine, then return the data. 
+ 		if (!error && response.statusCode == 200) {
+    		console.log(body) 
+    		var movie = JSON.parse(body); 
+    		console.log(movieTitle + "IMDB Rating:" + movie.imdbRating);
+  		}
+  		
+  		else{
+  			console.log(error);
+  		}
+	});
+};
+
+
+
+
+
+
