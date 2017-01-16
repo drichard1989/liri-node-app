afaaae54444186bcd6adad2e9a0aed236b7aef14
drichard1989 <drichard1989@icloud.com>
2017-01-16 14:26:41 -0600

    Fixed a index number issue for my switch case variables, fixing the problem of a broken return for OMDB. Shouldnt have to deal with that anymore.

--- request.js
+++ request.js
@@ -4,20 +4,22 @@ var request = require("request");
 var Twitter = require("twitter");
 
 
-var action = process.argv[2];
+var sourceRequest = process.argv[2];
 var input = process.argv[3];
 
 // Lets create a switch case for multiple requests from the same document.
 
-switch (input){
+switch (sourceRequest){
 	case "twitter":
 		tweetReturns();
+		console.log("Seeing SwitchCase")
 		break;
 
 	case "OMDB":
 		OMDBRequest();
+		console.log("Seeing SwitchCase")
 		break;
-};
+}
 
 
 // This is the function that requests the information from OMDB
@@ -31,6 +33,8 @@ function OMDBRequest(){
 	movieTitle = movieTitle + process.argv[i] + " ";
 	};	
 
+	console.log(movieTitle);
+
 	// Setting the variable for the API call 
 	var omdbAPIURL = "http://omdbapi.com?t=" + movieTitle + "&r=json";
 
@@ -42,7 +46,7 @@ function OMDBRequest(){
     		var movie = JSON.parse(body); 
     		console.log(movieTitle + "IMDB Rating:" + movie.imdbRating);
   		}
-  		
+
   		else{
   			console.log(error);
   		}
