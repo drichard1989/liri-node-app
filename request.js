03d7b6c7cebd5dbbba801592087b739c426bdc3e
drichard1989 <drichard1989@icloud.com>
2017-01-16 18:59:11 -0600

    Completed the ability to pass a movie name into the command line after specifying the movie return and have it return data. Now I need to add more content that is returned, specifically all the movie data the homework asks for.

--- request.js
+++ request.js
@@ -12,28 +12,31 @@ var input = process.argv[3];
 switch (sourceRequest){
 	case "twitter":
 		tweetReturns();
-		console.log("Seeing SwitchCase")
+
 		break;
 
-	case "OMDB":
-		OMDBRequest();
-		console.log("Seeing SwitchCase")
+	case "movie-this":
+		movieThis();
+
 		break;
 }
 
 
 // This is the function that requests the information from OMDB
 
-function OMDBRequest(){
+function movieThis(){
 	// Setting the variable for URL integration
 	var movieTitle = "";
+	
 
 	// Creating a for loop so I can input multiple word movie titles...
 	for (i=3; i<process.argv.length; i++){
 	movieTitle = movieTitle + process.argv[i] + " ";
 	};	
 
-	console.log(movieTitle);
+	if(movieTitle == ""){
+		movieTitle = "Mr. Nobody";
+	};
 
 	// Setting the variable for the API call 
 	var omdbAPIURL = "http://omdbapi.com?t=" + movieTitle + "&r=json";
@@ -41,18 +44,19 @@ function OMDBRequest(){
 	// Requesting the information from OMDB
 	request(omdbAPIURL, function (error, response, body) {
 		// If no error, and response code is 200, which means works fine, then return the data. 
+		console.log("Movie Title: " + movieTitle);
+
  		if (!error && response.statusCode == 200) {
-    		console.log(body) 
-    		var movie = JSON.parse(body); 
-    		console.log(movieTitle + "IMDB Rating:" + movie.imdbRating);
-  		}
+			var movie = JSON.parse(body); 
+			console.log(movieTitle + " IMDB Rating: " + movie.imdbRating);
+		}
 
-  		else{
+		else{
   			console.log(error);
-  		}
-	});
-};
+  		};
 
+	});
+}
 
 
 
