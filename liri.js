5f2a020f28b7f520db154b79cb9cf03e596cfb2a
drichard1989 <drichard1989@icloud.com>
2017-01-17 21:21:19 -0600

    Created a spotify function that will take the information passed into the command line and return the data related to the search query using Spotify API.

--- liri.js
+++ liri.js
@@ -2,37 +2,43 @@
 
 var request = require("request");
 var Twitter = require("twitter");
+var Spotify = require("spotify");
 
 
 var sourceRequest = process.argv[2];
 var input = process.argv[3];
 
+for (i=4; i<process.argv.length; i++){
+	input += " " + process.argv[i];
+	};	
+
 // Lets create a switch case for multiple requests from the same document.
 
 switch (sourceRequest){
-	case "twitter":
-		tweetReturns();
-
+	case "my-tweets":
+		returnTweets();
 		break;
 
 	case "movie-this":
 		movieThis();
+		break;
 
+	case "spotify-this-song":
+		spotifyThis();
 		break;
 }
 
 
 // This is the function that requests the information from OMDB
-
 function movieThis(){
 	// Setting the variable for URL integration
-	var movieTitle = "";
+	var movieTitle = input;
+
+	console.log(movieTitle);
 	
 
 	// Creating a for loop so I can input multiple word movie titles...
-	for (i=3; i<process.argv.length; i++){
-	movieTitle = movieTitle + process.argv[i] + " ";
-	};	
+	
 
 	if(movieTitle == ""){
 		movieTitle = "Mr. Nobody";
@@ -64,6 +70,61 @@ function movieThis(){
   		};
 
 	});
+} //Closing OMDB Moviethis function
+
+function returnTweets() {
+	
+	// This pulls the keys from the keys.js file that were requested from Twitter.
+	var twitterKeys = require("./keys.js").twitterKeys;
+
+	// This re-assigns the values returned to variables to be used here.
+	var consumer_key = twitterKeys['consumer_key'];
+	var consumer_secret = twitterKeys['consumer_secret'];
+	var access_token_key = twitterKeys['access_token_key'];
+	var access_token_secret = twitterKeys['access_token_secret'];
+
+
+	// This makes a new object 
+	var client = new Twitter({
+		consumer_key: consumer_key,
+		consumer_secret: consumer_secret,
+		access_token_key: access_token_key,
+		access_token_secret: access_token_secret
+	})
+
+	client.get('search/tweets', {q: 'jtimberlake', count: 20}, function(error, tweets, response){
+
+		if(error){
+			console.log(error);
+			throw error;
+		}
+
+		else{
+
+			var tweetContent = "";
+			for (i = 0; i < tweets.statuses.length; i++){
+				tweetContent += '\n' + tweets.statuses[i].text + '\n';
+			}
+			console.log(tweetContent);
+
+		}
+	})
+
+}
+
+function spotifyThis(){
+
+	Spotify.search({ type: 'track', query: input }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    else{
+    	console.log(data);
+    	console.log(input);
+    }
+});
 }
 
 
